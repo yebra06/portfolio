9a97661ed707522a4c074693ba8a279f7563744b
yebra06 <fred.yebra@gmail.com>
2017-05-24 17:08:23 -0500

    Add sass compiler and styles directory.

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,14 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ],
     },
@@ -31,11 +39,13 @@ module.exports = {
         port: 3001
     },
     plugins: [
+        // Use to server bundles in html.
+        // See: https://www.npmjs.com/package/html-webpack-plugin
         new HtmlWebpackPlugin({
             title: 'Alfredo\'s Portfolio',
             template: path.resolve(APP_DIR, 'index.html'),
             filename: 'index.html',
             inject: true
-        })
+        }),
     ]
 };
