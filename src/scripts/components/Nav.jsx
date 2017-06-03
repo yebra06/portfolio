9b057e49e21f780b766ea541ae3ba63dc94a5040
yebra06 <fred.yebra@gmail.com>
2017-06-02 20:57:52 -0500

    Make navbar responsive.

--- src/scripts/components/Nav.jsx
+++ src/scripts/components/Nav.jsx
@@ -6,28 +6,30 @@ import About from '../components/About.jsx';
 import Home from '../components/Home.jsx';
 
 export default class Nav extends Component {
+    constructor() {
+        super();
+    }
+
     render() {
         return (
             <div>
                 <nav className="navbar">
                     <div className="navbar-section">
-                        <div className="brand">
-                            <Link to="/" className="nav-link show-mobile-md">AYJ</Link>
-                            <Link to="/" className="nav-link hide-mobile-md">Alfredo Yebra Jr.</Link>
-                        </div>
+                        <Link to="/" className="nav-link show-mobile-md">AYJ</Link>
+                        <Link to="/" className="nav-link hide-mobile-md">Alfredo Yebra Jr.</Link>
                     </div>
                     <div className="navbar-section hide-mobile-md">
                         <Link className="nav-link" to="/about">About</Link>
                         <Link className="nav-link" to="/">Projects</Link>
                         <Link className="nav-link" to="/">Work</Link>
                     </div>
-                    <div className="navbar-section hide-mobile-md">
-                        <a className="social-icon" href="https://github.com/yebra06">
-                            <FontAwesome name='github' size='lg'/>
-                        </a>
-                        <a className="social-icon" href="https://www.linkedin.com/in/alfredo-yebra-jr-688257116/">
-                            <FontAwesome name='linkedin' size='lg'/>
-                        </a>
+                    <div className="navbar-section social-icons hide-mobile-md">
+                            <a className="social-icon" href="https://github.com/yebra06">
+                                <FontAwesome name='github' size='lg'/>
+                            </a>
+                            <a className="social-icon" href="https://www.linkedin.com/in/alfredo-yebra-jr-688257116/">
+                                <FontAwesome name='linkedin' size='lg'/>
+                            </a>
                     </div>
                     <div className="navbar-section show-mobile-md">
                         <div className="hamburger">
